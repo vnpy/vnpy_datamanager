d2559b7223fba4ffeb209bbb5d8b4246e34de20f
vn.py <xiaoyou.chen@foxmail.com>
2023-09-08 15:43:12 +0800

    [Fix] 修复数据区间选择对话框中，日期偏差1天的问题

--- vnpy_datamanager/ui/widget.py
+++ vnpy_datamanager/ui/widget.py
@@ -426,14 +426,14 @@ class DateRangeDialog(QtWidgets.QDialog):
             QtCore.QDate(
                 start.year,
                 start.month,
-                start.day
+                start.day + 1
             )
         )
         self.end_edit: QtWidgets.QDateEdit = QtWidgets.QDateEdit(
             QtCore.QDate(
                 end.year,
                 end.month,
-                end.day
+                end.day + 1
             )
         )
 
